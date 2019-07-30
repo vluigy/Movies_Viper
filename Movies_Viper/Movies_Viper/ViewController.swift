a777f5c1ec26b1cb3560fd6f489f6347a97fac5f
Luigy Francheskolly Vega Hernandez <luigy.vega@mdcloudps.com>
2019-07-30 11:22:10 -0500

    base project and gitignore

---
+++ Movies_Viper/Movies_Viper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movies_Viper
+//
+//  Created by Luigy Vega on 7/30/19.
+//  Copyright © 2019 Luigy Vega. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
